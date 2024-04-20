be4d9b36cb7b57be8f069e3191d9ef574b1502ba
babarali <babaraliengineer32@gmail.com>
2024-04-20 09:38:48 +0500

    numbr of prints added

--- src/jenki.java
+++ src/jenki.java
@@ -4,9 +4,10 @@ public class jenki {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello world!");
-            Thread.sleep(2000);
             System.out.print(i+ " ");
+            System.out.println("Hello world....!!!!");
+            Thread.sleep(2000);
+
         }
     }
 }
